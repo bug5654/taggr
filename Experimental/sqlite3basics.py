4f0f226adfad032d13e9ee54920f15d4dfd45bd3
dbugger <bug.new@gmail.com>
2018-09-21 18:38:32 -0700

    cursor.executemany() instead of cursor.execute()

--- Experimental/sqlite3basics.py
+++ Experimental/sqlite3basics.py
@@ -5,14 +5,18 @@ import sqlite3
 #DB in file
 db = sqlite3.connect('mydb')
 cursor = db.cursor()
-cursor.execute('''
-	CREATE TABLE users(id INTEGER PRIMARY KEY, name TEXT, phone TEXT,
-		email TEXT unique, password TEXT)
-''')
-cursor.execute('''
-	INSERT INTO users (id, name, phone, email, password) 
-	VALUES (1,"Gypsy","707","gypsy.agee@gmail.com", "meow") 
-''')
+#cursor.execute('''
+#	CREATE TABLE users(id INTEGER PRIMARY KEY, name TEXT, phone TEXT,
+#		email TEXT unique, password TEXT)
+#''')
+users = [
+	('hiro', '707', 'hiro.agee@gmail.com', 'meow'),
+	('stranger', '714', 'stranger@gmail.com', 'strangeThingsAreHappeningToMe')
+	]
+cursor.executemany('''
+	INSERT INTO users ( name, phone, email, password) 
+	VALUES (?,?,?,?) 
+''', users)
 db.commit()		#notice it's the DB that commits, not the cursor
 #when finished
 db.close()
